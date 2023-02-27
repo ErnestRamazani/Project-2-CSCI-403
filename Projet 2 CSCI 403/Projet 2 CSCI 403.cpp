71dca0deb3c6b80e4ab2ddc4a63152df5658e906
Josh Castagna <JoshuaCastagna@outlook.com>
2023-02-27 12:52:35 -0500

    beginning for loop creation

--- Projet 2 CSCI 403/Projet 2 CSCI 403.cpp
+++ Projet 2 CSCI 403/Projet 2 CSCI 403.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <thread>
-#include <pthread.h>
+//#include <pthread.h>
 #include "matrix.h"
 
 using namespace std;
@@ -15,25 +15,27 @@ int main()
 {
     int row, col;
     double ** mat_A, ** mat_B,**mat_C;
+    double sum; //sum 
 
     mat_A = read2d("a.mat", row, col); 
     mat_B = read2d("b.mat", row, col);
 
     print2d("\nMatrix A ", mat_A, row, col); 	// printing matrices
     print2d("\nMatrix B ", mat_B, row, col);
-    
     //multiplying matrices 
 
-    thread threads[THREADS_NUM];
+   thread threads[THREADS_NUM];
+   // for loop to create threads
+   // need to figure out how to divide threads and multiply matrix
+
     for (int i = 0; i < THREADS_NUM; i++) 
     {
-        double* x;
+      //for(int j = 0; j < )
         threads[i] = thread(mat_multiplication());
+
     }
 
     //joining threads
-
-
     free2d(mat_A); 			// deallocate the dynamic memory
     free2d(mat_B);
     
@@ -41,9 +43,9 @@ int main()
 
 }
 
-void* mat_multiplication(int r, int c, double **A, double **B) {
+void* mat_multiplication() {
 
+    
 
-
-
+    
 }
