58bbe40aefe600ef61c9ce4a2513d8ec547f1b68
Josh Castagna <JoshuaCastagna@outlook.com>
2023-02-26 21:58:44 -0500

    Add project files.

---
+++ Projet 2 CSCI 403/Projet 2 CSCI 403.cpp
@@ -0,0 +1,49 @@
+
+
+#include <iostream>
+#include <thread>
+#include <pthread.h>
+#include "matrix.h"
+
+using namespace std;
+const int THREADS_NUM = 20;
+const int MAT_SIZE = 1028;
+
+void* mat_multiplication();
+
+int main()
+{
+    int row, col;
+    double ** mat_A, ** mat_B,**mat_C;
+
+    mat_A = read2d("a.mat", row, col); 
+    mat_B = read2d("b.mat", row, col);
+
+    print2d("\nMatrix A ", mat_A, row, col); 	// printing matrices
+    print2d("\nMatrix B ", mat_B, row, col);
+    
+    //multiplying matrices 
+
+    thread threads[THREADS_NUM];
+    for (int i = 0; i < THREADS_NUM; i++) 
+    {
+        double* x;
+        threads[i] = thread(mat_multiplication());
+    }
+
+    //joining threads
+
+
+    free2d(mat_A); 			// deallocate the dynamic memory
+    free2d(mat_B);
+    
+    return 0;
+
+}
+
+void* mat_multiplication(int r, int c, double **A, double **B) {
+
+
+
+
+}
