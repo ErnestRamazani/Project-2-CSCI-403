77e21dae2e2a2baf424316c5e52bc838ad987f83
Josh Castagna <JoshuaCastagna@outlook.com>
2023-02-28 23:28:05 -0500

    fixed matrix multiplication algorithm

--- Projet 2 CSCI 403/Projet 2 CSCI 403.cpp
+++ Projet 2 CSCI 403/Projet 2 CSCI 403.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <thread>
 #include <algorithm>
+#include <cmath>
 #include <chrono>
 #include "matrix.h"
 
@@ -16,44 +17,46 @@ void func1();
 int main()
 {
 
-    auto start = chrono::steady_clock::now();
+    auto start = chrono::steady_clock::now(); // start time
     int row, col, r = 0; //r is row counter for for loop 
     double ** mat_A, ** mat_B,**mat_C;
-    double sum = 0, st_dev,avg; //sum 
+    double sum = 0, st_dev = 0,avg = 0; //sum 
 
     mat_A = read2d("a.mat", row, col); 
     mat_B = read2d("b.mat", row, col);
     mat_C = allocate2d(row, col);
 
+    //DEBUG SORRY
+   
 
     print2d("\nMatrix A ", mat_A, row, col); 	// printing matrices
     print2d("\nMatrix B ", mat_B, row, col);
-    //multiplying matrices 
+   
+
 
    thread threads[THREADS_NUM];
    // for loop to create threads
-   // need to figure out how to divide threads and multiply matrix
-
     for (int i = 0; i < THREADS_NUM; i++) 
     {
         for (int j = r*(row/THREADS_NUM) ; j < min((r+1)*(row/THREADS_NUM),row);j++) 
         {
-            threads[i] = thread(mat_multiplication,mat_A,mat_B,mat_C,row,row,ref(sum));
-            threads[i].join();
+            threads[i] = thread(mat_multiplication,mat_A,mat_B,mat_C,r,row,ref(sum));
+            threads[i].join(); // <<-- feels wrong to place this here, but will abort without
             r++; //row increment
-            //add parallel calculations of avg, standard dev, etc. 
         }  
-        //threads[i].join();
     }  
 
     write2d("TEST", mat_C, row, col);
-    print2d("\nMatrix C", mat_C, row, col);
-   
+   print2d("\nMatrix C", mat_C, row, col);
+    cout << "\n SUM: " << sum << endl;
+    cout << "\n AVERAGE: " << avg << endl;
+    cout << "\n STANDARD DEVIATION: " << st_dev << endl;
+
 
     free2d(mat_A); 			// deallocate the dynamic memory
     free2d(mat_B);
     free2d(mat_C);
-    auto end = chrono::steady_clock::now();
+    auto end = chrono::steady_clock::now(); //end time
 
     cout << "\n Elapsed time: " << chrono::duration_cast<chrono::microseconds>(end - start).count() << " microseconds";
     return 0;
@@ -69,11 +72,13 @@ void func1() {
 
 void mat_multiplication(double ** A, double **B, double **C, int row, int size, double &sum ) {
     double temp = 0;
-    for (int i = 0; i < size-1; i++) { 
-        for (int j = 0; j < size- 1; j++) {
-            temp += A[row - 1][i] * B[i][j];
-         }
-       C[row - 1][i] = temp;
-       sum += C[row-1][i];
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+           // cout << temp << " = " << A[row - 1][i] << "*" << B[i][j] << "\n\n";
+            temp += A[row][i] * B[i][j];
+        }
+      //  cout << "C[" << row << "][" << i << "] = " << temp << endl;
+       C[row][i] = temp;
+       sum += C[row][i];
     }
 }
