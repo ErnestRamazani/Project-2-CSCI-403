03fff65bbecf900690418ddaee5c83681cfbfb32
Josh Castagna <JoshuaCastagna@outlook.com>
2023-02-28 22:38:03 -0500

    compiles without error, needs to add more, unsure about matrix multiplication

--- Projet 2 CSCI 403/Projet 2 CSCI 403.cpp
+++ Projet 2 CSCI 403/Projet 2 CSCI 403.cpp
@@ -17,10 +17,12 @@ int main()
 {
     int row, col, r = 0; //r is row counter for for loop 
     double ** mat_A, ** mat_B,**mat_C;
-    double sum, st_dev,avg; //sum 
+    double sum = 0, st_dev,avg; //sum 
 
     mat_A = read2d("a.mat", row, col); 
     mat_B = read2d("b.mat", row, col);
+    mat_C = allocate2d(row, col);
+
 
     print2d("\nMatrix A ", mat_A, row, col); 	// printing matrices
     print2d("\nMatrix B ", mat_B, row, col);
@@ -34,23 +36,23 @@ int main()
     {
         for (int j = r*(row/THREADS_NUM) ; j < min((r+1)*(row/THREADS_NUM),row);j++) 
         {
-            threads[i] = thread(/* function and params supposed to go here?*/);
+            threads[i] = thread(mat_multiplication,mat_A,mat_B,mat_C,row,row,ref(sum));
+            threads[i].join();
             r++; //row increment
             //add parallel calculations of avg, standard dev, etc. 
         }  
-    }
+        //threads[i].join();
+    }  
 
-
-    //joining threads
-    for (int i = 0; i < THREADS_NUM; i++) {
-        threads[i].join();
-    }
-
-    //print2d("\nMatrix C", mat_C, row, col);
+   // write2d("TEST", mat_C, row, col);
+    print2d("\nMatrix C", mat_C, row, col);
+   
 
     free2d(mat_A); 			// deallocate the dynamic memory
     free2d(mat_B);
-  //  free2d(mat_C);
+    free2d(mat_C);
+    
+    
     return 0;
 
 }
@@ -63,11 +65,12 @@ void func1() {
 
 
 void mat_multiplication(double ** A, double **B, double **C, int row, int size, double &sum ) {
-
-    for (int i = 0; i < size; i++) { 
-        for (int j = 0; j < size; j++) {
-            C[row][i] += A[row][i] * B[i][j];
+    double temp = 0;
+    for (int i = 0; i < size-1; i++) { 
+        for (int j = 0; j < size- 1; j++) {
+            temp += A[row - 1][i] * B[i][j];
          }
-        sum += C[row][i];
+       C[row - 1][i] = temp;
+       sum += C[row-1][i];
     }
 }
