51d7f701aafb58bad2cec1f88506ed85d69e96d5
Josh Castagna <JoshuaCastagna@outlook.com>
2023-03-02 00:10:24 -0500

    * matrix multiplication function threads skip random rows

--- Projet 2 CSCI 403/Projet 2 CSCI 403.cpp
+++ Projet 2 CSCI 403/Projet 2 CSCI 403.cpp
@@ -4,10 +4,11 @@
 #include <thread>
 #include <cmath>
 #include <chrono>
+#include <fstream>
 #include "matrix.h"
 
 using namespace std;
-const int THREADS_NUM = 5; //THREAD COUNT
+const int THREADS_NUM = 10; //THREAD COUNT
 
 void mat_multiplication(double** A, double** B, double** C, int row, int size, double& sum, double& avg, double& stdev); //need to figure out how to calculate matrix multiplication
 
@@ -18,12 +19,27 @@ int main()
     double ** mat_A, ** mat_B,**mat_C;
     double sum = 0, st_dev = 0,avg = 0; //sum 
     double sum_ar[THREADS_NUM], st_dev_ar[THREADS_NUM], avg_ar[THREADS_NUM]; //arrays, index for each thread to add together after all threads are finished
+    thread threads[THREADS_NUM];
+
     for (int i = 0; i < THREADS_NUM; i++) {
         sum_ar[i] = 0;
         st_dev_ar[i] = 0;
         avg_ar[0] = 0;
     }
-    double average = 0;
+    
+    auto lambda = [](int& r, int row, double** mat_A, double** mat_B, double** mat_C, int thread, double* sum_ar, double* avg_ar, double* st_dev_ar) {
+
+        double sum = 0, st_dev = 0, avg = 0;
+
+        for (int j = r * (row / THREADS_NUM); j < (min((r + 1) * (row / THREADS_NUM), row)) - 1; j++) {
+
+            mat_multiplication(mat_A, mat_B, mat_C, r, row, ref(sum), ref(avg), ref(st_dev));
+            if (r < row - 1) {
+                r++;
+            }
+        }
+        cout << "THREAD " << thread << " DONE!!" << endl;
+    };
 
     mat_A = read2d("a.mat", row, col); 
     mat_B = read2d("b.mat", row, col);
@@ -32,27 +48,6 @@ int main()
     print2d("\nMatrix A ", mat_A, row, col); 	// printing matrices
     print2d("\nMatrix B ", mat_B, row, col);
 
-   thread threads[THREADS_NUM];
-
-   auto lambda = [](int& r, int row, double** mat_A, double** mat_B, double** mat_C,int thread,double* sum_ar,double* avg_ar, double* st_dev_ar) {
-       
-       double sum = 0, st_dev = 0, avg = 0;
-
-       for (int j = r * (row / THREADS_NUM); j < (min((r + 1) * (row / THREADS_NUM), row))-1; j++) {
-        //   cout << "rows " << r * (row / THREADS_NUM) << "-" << min((r + 1) * (row / THREADS_NUM), row) << endl;
-
-           mat_multiplication(mat_A, mat_B, mat_C, r, row, ref(sum), ref(avg), ref(st_dev));
-           sum_ar[thread] += sum;
-           avg_ar[thread] += avg;
-           st_dev_ar[thread] += st_dev;
-
-           if (r < row - 1) {
-               r++;
-           }
-       }
-       cout << sum_ar[thread] << endl;
-       cout << "THREAD " << thread << " DONE!!" << endl;
-   };
    // for loop to create threads
     for (int i = 0; i < THREADS_NUM; i++) 
     {
@@ -67,23 +62,84 @@ int main()
         // r = current row
         // row = row size
         threads[i] = thread(lambda, ref(r), row, mat_A, mat_B, mat_C,i,sum_ar, avg_ar,st_dev_ar);
-        
     }  
  
     for (int i = 0; i < THREADS_NUM; i++) {
         threads[i].join();
     }
+
+
     for (int i = 0; i < THREADS_NUM; i++) {
         sum += sum_ar[i];
         avg += avg_ar[i];
         st_dev += st_dev_ar[i];
     }
 
+    // CALCULATING SUM, AVG, AND STANDARD DEVIATION NON-PARALLEL >>>>>>>>>>>
+    // note: these do not seem to work, either
+    double realsum = 0;
+    double realavg = 0;
+    double realstdev = 0;
+    
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row; j++) {
+            realsum += mat_C[i][j]; 
+            realstdev += pow(mat_C[i][j], 2) / (row * col);
+        }
+    }
+
+    realavg = realsum / (row * col);
+    realstdev = sqrt(realstdev - pow(realavg, 2));
+
+    cout << "======== CORRECT CALCULATED DATA =========" << endl;
+    cout << "SUM: " << realsum << endl;
+    cout << "MEAN: " << realavg << endl;
+    cout << "STDEV: " << realstdev << endl;
+    cout << "==========================================" << endl;
+
+
+    ofstream myfile;
+    myfile.open("matC2.csv");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            myfile << mat_C[i][j] << ",";
+        }
+        myfile << "\n";
+    }
+    myfile.close();
+
+    myfile.open("matAB.csv");
+    myfile << "MATRIX A \n";
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            myfile << mat_A[i][j] << ",";
+        }
+        myfile << "\n";
+    }
+    myfile << "\n MATRIX B \n";
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            myfile << mat_B[i][j] << ",";
+        }
+        myfile << "\n";
+    }
+    myfile.close();
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    
     print2d("\nMatrix C", mat_C, row, col);
     cout << "\n SUM: " << sum << endl;
     cout << "\n AVERAGE: " << avg << endl;
     st_dev = sqrt(st_dev - pow(avg,2)); //standard deviation
-    cout << "\n STANDARD DEVIATION: " << st_dev << endl;
+    cout << "\n STANDARD DEVIATION: " << st_dev << endl ;
 
     free2d(mat_A); 			// deallocate the dynamic memory
     free2d(mat_B);
